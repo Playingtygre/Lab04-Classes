1517c5c3078daea5df12a52399a91586b4a034cd
Playingtygre <kymiehsu123@gmail.com>
2018-03-25 23:58:18 -0700

    Working on the backing store, adding diffrernt functionality with under standing how classes work

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -4,9 +4,45 @@ namespace TicTacToe
 {
     class Program
     {
+
+       
+     
+   
         static void Main(string[] args)
         {
+            
+            
+
             Console.WriteLine("Hello World!");
+
+            //Type name = new type
+            // tiger is a new object of the person class   
+            Person tiger = new Person();
+
+
+            Person jeannie = new Person();
+            jeannie.SayHi();
+
+            tiger.Dance();
+
+            tiger.Name = "Hsu"; // set grabing the value of the name
+            string name = tiger.Name; //grabing the value of the name
+
+            Console.WriteLine(name);
+
+
+            Console.WriteLine("Tiger " + tiger.SayHi());
+            Console.WriteLine("Jeannie" + jeannie.SayHi());
+            Console.WriteLine(tiger.Goodbye());
+            Console.ReadLine();
+
+
+
         }
+
+        
+
+
+
     }
 }
